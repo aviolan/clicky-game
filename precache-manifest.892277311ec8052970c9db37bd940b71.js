a62c99ecc0dc84b5a111d0aada68b4bc70cdef4e
Andrew Violan <andrew.violan@gmail.com>
2019-02-13 18:11:19 -0800

    Updates

---
+++ precache-manifest.892277311ec8052970c9db37bd940b71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "8c167d0ea204f10f9deb",
+    "url": "/clicky-game/static/js/main.8c167d0e.chunk.js"
+  },
+  {
+    "revision": "dcb82aee628a7a110fea",
+    "url": "/clicky-game/static/js/2.dcb82aee.chunk.js"
+  },
+  {
+    "revision": "8c167d0ea204f10f9deb",
+    "url": "/clicky-game/static/css/main.af4f5c49.chunk.css"
+  },
+  {
+    "revision": "9db26e3aa6cf2e2f0cfa05190b78b114",
+    "url": "/clicky-game/index.html"
+  }
+];
